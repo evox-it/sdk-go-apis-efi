bbc9d41ece4b8b212143b65adf48c161eb2ac0e2
celso-alexandre <celsoalexandre@live.com>
2025-01-09 15:01:47 -0300

    feat: Allow to pass use Pix struct outside the lib

--- src/efipay/pix/efiPay_pix.go
+++ src/efipay/pix/efiPay_pix.go
@@ -1,10 +1,10 @@
 package pix
 
-type pix struct {
+type Pix struct {
 	endpoints
 }
 
-func NewEfiPay(configs map[string]interface{}) *pix {
+func NewEfiPay(configs map[string]interface{}) *Pix {
 	clientID := configs["client_id"].(string)
 	clientSecret := configs["client_secret"].(string)
 	CA := configs["CA"].(string)
@@ -12,8 +12,8 @@ func NewEfiPay(configs map[string]interface{}) *pix {
 	sandbox := configs["sandbox"].(bool)
 	timeout := configs["timeout"].(int)
 
-	requester := newRequester(clientID, clientSecret,CA, Key, sandbox, timeout)
-	efi := pix{}
+	requester := newRequester(clientID, clientSecret, CA, Key, sandbox, timeout)
+	efi := Pix{}
 	efi.requester = *requester
 	return &efi
 }
